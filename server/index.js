2362ca5f897bac413ac2f0a7a23bf0b01f280044
fhelwanger <fhelwanger@gmail.com>
2015-10-11 00:28:57 -0300

    Client-side config

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ var express = require('express');
 var app = express();
 var db = require('./db');
 
-app.get('/', function (req, res) {
-  db.Animais.findAll().then(function (resp) {
-    res.json(resp);
-  });
-});
+app.use(express.static('client/dist'));
+
+// app.get('/', function (req, res) {
+//   db.Animais.findAll().then(function (resp) {
+//     res.json(resp);
+//   });
+// });
 
 db.sequelize.sync().then(function () {
   var server = app.listen(1337, function () {
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: path.join(__dirname, 'client/src/index.js'),
+  output: {
+    path: path.join(__dirname, 'client/dist/js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel' }
+    ]
+  }
+};
