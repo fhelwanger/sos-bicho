eea6f235b4227e12f7870f6258554711e3a3e5be
fhelwanger <fhelwanger@gmail.com>
2015-11-09 19:51:02 -0200

    Add compression

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
+var compression = require('compression');
 var app = express();
 var api = express.Router();
 
 app.set('port', process.env.PORT || 1337);
 
+app.use(compression());
 app.use(express.static('client/dist'));
 app.use(bodyParser.json({ limit: '20mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '20mb' }));
